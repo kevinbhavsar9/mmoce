a8e0d2eba8b19162c38d0623373c4d0154019f69
kevin bhavsar <kevinbhavsar567@gmail.com>
2022-01-09 15:35:13 +0530

    confogure backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productsRoutes.js";
 
+//in ES style file must have dotjs behind their name
+// const products = require("./data/products");
+import products from "./data/products.js";
+dotenv.config();
+
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
-app.listen(5000, console.log("Server running on port 5000"));
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
