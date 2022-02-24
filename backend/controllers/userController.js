0ec5932bdb05f43f949d61781cd06ef3e57614bb
kevin bhavsar <kevinbhavsar567@gmail.com>
2022-02-24 17:35:49 +0530

    added admin middleware and getuser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -99,4 +99,13 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile };
+//@desc    Get all users
+//@route   GET /api/users
+//@access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  //console.log(req.user);
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
